fee80e7191ebc703ac1c6cd8b779684527ae4242
Tuğba Yıldız <78541390+badnothendd@users.noreply.github.com>
2021-08-19 12:41:09 +0300

    Add files via upload

---
+++ array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	int sayilar[] = {1,2,3,4,5};
+	int i=0;
+	
+	 for (; i<5; i++) {
+	 	printf("%d\n",sayilar[i]);
+	 }	
+	
+	return 0;
+}
